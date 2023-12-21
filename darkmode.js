463cd88dac270b9c709d84f0635bb808fa0350d2
Iulian Ursu <141002129+iulicc@users.noreply.github.com>
2023-12-21 15:16:59 +0200

    Update darkmode.js

--- darkmode.js
+++ darkmode.js
@@ -1,22 +1,23 @@
-console.log("Script loaded.");
+console.log("Script loaded!!!!!");
 
-// First, we wait for the DOM to be fully loaded
+// Wait for the DOM to be fully loaded
 document.addEventListener('DOMContentLoaded', function() {
     // Find the form element by its ID
     var form = document.getElementById('form');
 
     // Check if the form exists to avoid null reference errors
     if (form) {
-        // Find the div with data-role="page-header" inside the form
-        var pageHeaderDiv = form.querySelector('div[data-role="page-header"]');
+        // Find the button with id="theme-btn" inside the form
+        var themeButton = form.querySelector('button#theme-btn');
 
-        // Check if the div exists inside the form
-        if (pageHeaderDiv) {
-            // Add a click event listener to the div
-            pageHeaderDiv.addEventListener('click', function() {
-                // When the div is clicked, log 'Hello World' to the console
+        // Check if the button exists inside the form
+        if (themeButton) {
+            // Add a click event listener to the button
+            themeButton.addEventListener('click', function() {
+                // When the button is clicked, log 'Hello World' to the console
                 console.log('Hello World');
             });
         }
     }
 });
+
