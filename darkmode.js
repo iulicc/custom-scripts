3e1b8b0b5e78efde16209b654aa6fdb98ef9711b
Iulian Ursu <141002129+iulicc@users.noreply.github.com>
2023-12-21 15:32:27 +0200

    Update darkmode.js

--- darkmode.js
+++ darkmode.js
@@ -1,13 +1,3 @@
 console.log("Script loaded!!!!!");
-
-
-            var element = document.getElementById("htmlblock-00000010-acc");
-
-            // Check if the element exists
-            if (element) {
-                // Console log the element
-                console.log(element);
-            } else {
-                console.log("Element not found.");
-            }
+console.log("Hello world!");
 
