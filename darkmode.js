fa64e2aa723ba9852192816577d9d5a17fae7b13
Iulian Ursu <141002129+iulicc@users.noreply.github.com>
2023-12-21 14:53:52 +0200

    Update darkmode.js

--- darkmode.js
+++ darkmode.js
@@ -1,10 +1,1 @@
 console.log("Script loaded.");
-
-document.addEventListener("DOMContentLoaded", function() {
-    var form = document.getElementById("form");
-    var button = form.querySelector('div[data-id="111052249"] button');
-
-    button.addEventListener("click", function() {
-        console.log("Hello World");
-    });
-});
