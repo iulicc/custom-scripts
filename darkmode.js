938ed52253a7a9851eb2637142a5cb29aaa8f873
Iulian Ursu <141002129+iulicc@users.noreply.github.com>
2023-12-21 15:05:21 +0200

    Update darkmode.js

--- darkmode.js
+++ darkmode.js
@@ -1,1 +1,23 @@
-console.log("Script loaded!!!");
+console.log("Script loaded.");
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Get the iframe with the specified class
+    var iframe = document.querySelector('.preview-app__iframe__iframe-view');
+
+    // Check if the iframe is available
+    if (iframe) {
+        // Access the contentDocument of the iframe
+        var iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
+
+        // Access elements within the iframe
+        var form = iframeDocument.getElementById("form");
+        var button = form.querySelector('div[data-id="111052249"] button');
+
+        // Add a click event listener to the button inside the iframe
+        button.addEventListener("click", function() {
+            console.log("Hello World");
+        });
+    } else {
+        console.log("Iframe not found.");
+    }
+});
